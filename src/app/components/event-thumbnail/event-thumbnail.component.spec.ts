2916f11ab6716aff6a9df42514f1d713a02a9b02
dhouha <dhouha.mansour11@gmail.com>
2022-03-26 12:19:54 +0100

    unit test

--- src/app/components/event-thumbnail/event-thumbnail.component.spec.ts
+++ src/app/components/event-thumbnail/event-thumbnail.component.spec.ts
@@ -8,9 +8,8 @@ describe('EventThumbnailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EventThumbnailComponent ]
-    })
-    .compileComponents();
+      declarations: [EventThumbnailComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +20,27 @@ describe('EventThumbnailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('getStartTimeClass', () => {
+    it('Test should return green and bold if time is 8 am', () => {
+      component.eventThumbnail = {
+        date: '2/10/2037',
+        id: 5,
+        time: '8:00 am',
+      };
+
+      expect(component.getStartTimeClass()).toEqual(['green', 'bold']);
+    });
+
+    it('Test should return empty if time is 8 am', () => {
+      component.eventThumbnail = {
+        date: '2/10/2037',
+        id: 5,
+        time: '9:00 am',
+      };
+
+      expect(component.getStartTimeClass()).toEqual([]);
+    });
   });
 });
