70c9e046a73582229eee9ce7d28e579665247cee
dhouha <dhouha.mansour11@gmail.com>
2022-04-19 09:50:50 +0200

    test

--- src/app/components/event-thumbnail/event-thumbnail.component.spec.ts
+++ src/app/components/event-thumbnail/event-thumbnail.component.spec.ts
@@ -25,20 +25,64 @@ describe('EventThumbnailComponent', () => {
   describe('getStartTimeClass', () => {
     it('Test should return green and bold if time is 8 am', () => {
       component.eventThumbnail = {
-        date: '2/10/2037',
-        id: 5,
+        id: 1,
+        name: 'Angular Connect',
+        date: new Date('9/26/2036'),
         time: '8:00 am',
-      };
+        price: 599.99,
+        imageUrl: '/assets/images/angularconnect-shield.png',
+        location: {
+          address: '1057 DT',
+          city: 'London',
+          country: 'England',
+        },
+        sessions: [
+          {
+            id: 1,
+            name: 'Using Angular 4 Pipes',
+            presenter: 'Peter Bacon Darwin',
+            duration: 1,
+            level: 'Intermediate',
+            abstract: `Learn all about the new pipes in Angular 4, both 
+            how to write them, and how to get the new AI CLI to write 
+            them for you. Given by the famous PBD, president of Angular 
+            University (formerly Oxford University)`,
+            voters: ['bradgreen', 'igorminar', 'martinfowler'],
+          }
+        ],
+      }
 
       expect(component.getStartTimeClass()).toEqual(['green', 'bold']);
     });
 
-    it('Test should return empty if time is 8 am', () => {
-      component.eventThumbnail = {
-        date: '2/10/2037',
-        id: 5,
+    it('Test should return empty if time is not 8 am', () => {
+     component.eventThumbnail = {
+        id: 1,
+        name: 'Angular Connect',
+        date: new Date('9/26/2036'),
         time: '9:00 am',
-      };
+        price: 599.99,
+        imageUrl: '/assets/images/angularconnect-shield.png',
+        location: {
+          address: '1057 DT',
+          city: 'London',
+          country: 'England',
+        },
+        sessions: [
+          {
+            id: 1,
+            name: 'Using Angular 4 Pipes',
+            presenter: 'Peter Bacon Darwin',
+            duration: 1,
+            level: 'Intermediate',
+            abstract: `Learn all about the new pipes in Angular 4, both 
+            how to write them, and how to get the new AI CLI to write 
+            them for you. Given by the famous PBD, president of Angular 
+            University (formerly Oxford University)`,
+            voters: ['bradgreen', 'igorminar', 'martinfowler'],
+          }
+        ],
+      }
 
       expect(component.getStartTimeClass()).toEqual([]);
     });
--- src/app/components/events-list/events-list.component.spec.ts
+++ src/app/components/events-list/events-list.component.spec.ts
@@ -1,16 +1,60 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 import { EventsListComponent } from './events-list.component';
 
 describe('EventsListComponent', () => {
   let component: EventsListComponent;
   let fixture: ComponentFixture<EventsListComponent>;
+  const paramsSubject = new BehaviorSubject({});
+  const eventsData = {
+    events: [
+      {
+        id: 1,
+        name: 'Angular Connect',
+        date: new Date('9/26/2036'),
+        time: '10:00 am',
+        price: 599.99,
+        imageUrl: '/assets/images/angularconnect-shield.png',
+        location: {
+          address: '1057 DT',
+          city: 'London',
+          country: 'England',
+        },
+        sessions: [
+          {
+            id: 1,
+            name: 'Using Angular 4 Pipes',
+            presenter: 'Peter Bacon Darwin',
+            duration: 1,
+            level: 'Intermediate',
+            abstract: `Learn all about the new pipes in Angular 4, both 
+        how to write them, and how to get the new AI CLI to write 
+        them for you. Given by the famous PBD, president of Angular 
+        University (formerly Oxford University)`,
+            voters: ['bradgreen', 'igorminar', 'martinfowler'],
+          },
+        ],
+      },
+    ],
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EventsListComponent ]
-    })
-    .compileComponents();
+      declarations: [EventsListComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: paramsSubject,
+              data: eventsData,
+            },
+          },
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +66,16 @@ describe('EventsListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('ngOnInit', () => {
+    it('should return events', () => {
+      expect(component.events).toBe(eventsData.events);
+    });
+  });
+
+  // describe('handleThumbnailClick', () => {
+  //   it('should ', () => {
+     
+  //   });
+  // });
 });
