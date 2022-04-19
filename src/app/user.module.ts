28e2198f064bb6e49b56f0e752cfb1158a530d81
dhouha <dhouha.mansour11@gmail.com>
2022-04-19 11:21:26 +0200

    login

--- src/app/user.module.ts
+++ src/app/user.module.ts
@@ -2,17 +2,23 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 const userRoutes = [
   {
     path: 'profile',
     component: ProfileComponent,
   },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(userRoutes)],
-  declarations: [ProfileComponent],
+  imports: [CommonModule, FormsModule, RouterModule.forChild(userRoutes)],
+  declarations: [ProfileComponent, LoginComponent],
   providers: [],
 })
 export class UserModule {}
