e088911e3a2720cfed16a117ecbeb1fc1f52d61b
dhouha <dhouha.mansour11@gmail.com>
2022-04-19 12:04:27 +0200

    login

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let routerSpy = { navigate: jasmine.createSpy('navigate') };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [FormsModule],
+      providers: [{ provide: Router, useValue: routerSpy }],
     })
     .compileComponents();
   });
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   userName;
   password
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login(formValues) {
-    console.log(formValues)
+    this.authService.loginUser(formValues.userName, formValues.password)
+    this.router.navigate(['events'])
+  }
+
+  cancel() {
+    this.router.navigate(['events']);
   }
 }
